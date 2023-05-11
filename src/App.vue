20ab96882028cce64ec0a228e15b8395b532b42a
BeatrizVenturi <venturibeatriz329@gmail.com>
2023-05-11 13:29:14 -0300

    1005pt1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-const ListaCompras = ref([
+
+import { ref } from 'vue'
+
+
+const listaCompras = ref([
   {
     id: 1,
     nome: 'Camiseta',
@@ -62,52 +65,70 @@ const ListaCompras = ref([
     quantidade: 0,
   }
 ])
-function somar(index) {
-  ListaCompras.value[index].quantidade++
+const carrinhos = ref({
+  items: [],
+  total: 0,
+
+})
+function incrementar(index) {
+  listaCompras.value[index].quantidade++
 }
-function diminuir(index) {
-  if (ListaCompras.value[index].quantidade > 0) {
-    ListaCompras.value[index].quantidade--
-  } else {
-    ListaCompras.value[index].quantidade = 0
+function decrementar(index) {
+  if (listaCompras.value[index].quantidade > 0) {
+    listaCompras.value[index].quantidade--
   }
 }
 function adicionar(index) {
-  carrrinhos.value.item.push(...ListaCompras.value(index))
-}
-const carrrinhos = ref({
-  item : [] ,
-  preco : 0,
-})
+  if (listaCompras.value[index].quantidade > 0) {
 
+    carrinhos.value.items.push(
+      { ...listaCompras.value[index], preco: listaCompras.value[index].quantidade * listaCompras.value[index].preco }
+    );
+    carrinhos.value.total = carrinhos.value.total + listaCompras.value[index].preco * listaCompras.value[index].quantidade
+  }
+}
+function carrinho(index) {
+
+}
 </script>
+
 <template>
-  <button>Adicionar No Carrinho</button>
-  <div class="itens">
+  <div class="produtos">
     <ul>
-      <li class="lista" v-for="(item, index) in ListaCompras" :key="item.id">
-        Item: {{ item.nome }} <br>
-        Código item: {{ item.id }} <br>
-        Preço R${{ item.preco.toFixed(2) }} <br>
-        Quantidade: {{ item.quantidade }}
-        <br>
-        <button @click="somar(index)">+</button>
-        <button @click="diminuir(index)">-</button>
-        <br>
-        <button @click="adicionar">Adicionar</button>
-        <br>
+      <li class="lista" v-for="(item, index) in listaCompras" :key="item.id">
+        <p> Item: {{ item.nome }} <br>
+          Preco: {{ item.preco }} <br>
+          ID: {{ item.id }} <br>
+          quantidade: {{ item.quantidade }}
+          <button @click="incrementar(index)">+</button>
+          <button @click="decrementar(index)">-</button>
+          <button @click="adicionar(index)">Adicionar</button>
+        </p>
       </li>
     </ul>
+    <hr>
   </div>
-<hr>
 
   <div class="carrinho">
     <ul>
-      <li >
+
+      <li v-for="item in carrinhos.items">
+        <p>Item: {{ item.nome }} <br>
+          Preco: {{ item.preco.toFixed(2) }} <br>
+          ID: {{ item.id }} <br>
+          Quantidade: {{ item.quantidade }} <br>
+          <button @click="RemoverCarrinho">Remover</button>
+        </p>
       </li>
+      <p> Total:{{ carrinhos.total.toFixed(2) }}</p>
+
+
     </ul>
   </div>
-
 </template>
 
-<style scoped></style>
+<style scoped>
+.carrinho {
+  padding-bottom: 10px;
+}
+</style>
